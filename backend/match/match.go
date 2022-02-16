0e87bccabbc8e6e126c286858cc43598a4fef0d4
M.A.A <muratardahanasik@gmail.com>
2022-02-16 22:34:35 +0300

    refactor Play function

--- backend/match/match.go
+++ backend/match/match.go
@@ -29,9 +29,7 @@ func Play(m Match) Match {
 	*/
 	probabilityNoGoal := float64(50)
 	probabilityHomeGoal := float64((float64(m.HomeTeam.Power) / float64(m.HomeTeam.Power+m.AwayTeam.Power)) * 100 / 2)
-	probabilityAwayGoal := float64(100 - float64(probabilityNoGoal) - float64(probabilityHomeGoal))
-
-	fmt.Printf("Prob Home Goal %f, Prob Away Goal %f", probabilityHomeGoal, probabilityAwayGoal)
+	// probabilityAwayGoal := float64(100 - float64(probabilityNoGoal) - float64(probabilityHomeGoal))
 
 	// initial scores
 	homeScore := 0
@@ -41,18 +39,15 @@ func Play(m Match) Match {
 	// simulate every 10min
 	for i := 0; i < 9; i++ {
 		randEvent := float64(rand.Intn(100))
-		fmt.Println("rand event", randEvent)
+
 		// no goal
 		if randEvent < probabilityNoGoal {
-
 		} else if (probabilityNoGoal <= randEvent) && (randEvent < probabilityNoGoal+probabilityHomeGoal) {
 			// home goal
 			homeScore += 1
-			fmt.Println("Home Goal")
 		} else {
 			// away goal
 			awayScore += 1
-			fmt.Println("Away Goal")
 		}
 	}
 
