31059862edeeaed06460c21fa8777e5148896155
M.A.A <muratardahanasik@gmail.com>
2022-02-17 23:59:41 +0300

    add playNextWeek and Reset button functionalities

--- frontend/src/components/LeagueInformationContainer.js
+++ frontend/src/components/LeagueInformationContainer.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import LeagueTable from "./LeagueTable";
 import MatchesTable from "./MatchesTable";
 import ChampionshipPrediction from "./ChampionshipPrediction";
 import { Button } from "react-bootstrap";
 import { Row, Col } from 'react-bootstrap';
 
+import axios from 'axios';
+
 
 function LeagueInformationContainer(props) {
+	const leagueComponentRef = useRef();
+	const matchesTableRef = useRef();
+
+	const playNextWeek = () => {
+		axios.post("http://localhost:8080/matches").then(
+			res => {
+				console.log(res.status);
+				console.log(res.data);
+				leagueComponentRef.current.update();
+			}
+		)
+	};
+
+	const resetLeague = () => {
+		axios.delete("http://localhost:8080/league-table").then(
+			res => {
+				console.log(res.status);
+				console.log(res.data);
+			}
+		)
+	};
+
+
 	return (
 		<div>
 			<Row>
 				<Col>
-					<LeagueTable />
+					<LeagueTable ref={leagueComponentRef} />
 				</Col>
 				<Col>
-					<MatchesTable />
+					<MatchesTable ref={matchesTableRef} />
 				</Col>
 				<Col>
 					<ChampionshipPrediction />
@@ -25,10 +50,10 @@ function LeagueInformationContainer(props) {
 					<Button variant="primary">Play All Weeks</Button>
 				</Col>
 				<Col>
-					<Button variant="info">Play Next Week</Button>
+					<Button variant="info" onClick={playNextWeek}>Play Next Week</Button>
 				</Col>
 				<Col>
-					<Button variant="danger">Reset</Button>
+					<Button variant="danger" onClick={resetLeague}>Reset</Button>
 				</Col>
 			</Row>
 		</div>
